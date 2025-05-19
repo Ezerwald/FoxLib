3261d13f1348d41b44183f7cc2da4acb1992469e
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-05-19 22:46:12 +0200

    Add category tabs to Library Page

--- FoxLib/LibraryPage.xaml.cs
+++ FoxLib/LibraryPage.xaml.cs
@@ -1,9 +1,13 @@
+using System.Collections.ObjectModel;
+
 namespace FoxLib;
 
 public partial class LibraryPage : ContentPage
 {
-	public LibraryPage()
+    public ObservableCollection<string> FilterTabs { get; set; } = new() { "All", "Active", "Finished", "New" };
+    public LibraryPage()
 	{
         InitializeComponent();
-	}
+        BindingContext = this;
+    }
 }
