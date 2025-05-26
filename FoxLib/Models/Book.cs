8324ab69aa5bb9298f33ce003ed9f18deaea8c16
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-05-26 13:22:58 +0200

    Create Book model

---
+++ FoxLib/Models/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+
+namespace FoxLib.Models
+{
+    public enum ReadingStatus
+    {
+        New,
+        Active,
+        Finished
+    }
+
+    public class Book
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+        public string CoverImageUrl { get; set; }
+
+        public ReadingStatus Status { get; set; }
+    }
+}
