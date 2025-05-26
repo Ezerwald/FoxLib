d51098abcb3e5a3ecea120e597277dd2a5074b3a
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-05-26 12:28:48 +0200

    Change filter tabs to buttons
    Now filter tabs in LibraryPage are no longer collection, but buttons.
    Changed due to error which occured when trying to run release

--- FoxLib/LibraryPage.xaml.cs
+++ FoxLib/LibraryPage.xaml.cs
@@ -4,13 +4,39 @@ namespace FoxLib;
 
 public partial class LibraryPage : ContentPage
 {
-    public ObservableCollection<string> FilterTabs { get; set; } = [];
-    public int FilterTabsCount;
+    private Button _selectedTab;
+
     public LibraryPage()
     {
-        FilterTabs = ["All", "Active", "Finished", "New"];
-
         InitializeComponent();
-        BindingContext = this;
+        SelectTab(TabAll); // default selected tab
+    }
+
+    private void OnTabClicked(object sender, EventArgs e)
+    {
+        if (sender is Button clickedTab)
+        {
+            SelectTab(clickedTab);
+
+            // Optionally filter your book list based on clickedTab.Text
+            string selectedCategory = clickedTab.Text;
+            Console.WriteLine($"Filtering for: {selectedCategory}");
+        }
+    }
+
+    private void SelectTab(Button newTab)
+    {
+        if (_selectedTab != null)
+        {
+            // Reset old tab style
+            _selectedTab.BackgroundColor = Application.Current.Resources["White"] as Color ?? Colors.White;
+            _selectedTab.TextColor = Application.Current.Resources["Black"] as Color ?? Colors.Black;
+        }
+
+        // Highlight new tab
+        newTab.BackgroundColor = Application.Current.Resources["Primary"] as Color ?? Colors.Blue;
+        newTab.TextColor = Application.Current.Resources["White"] as Color ?? Colors.White;
+
+        _selectedTab = newTab;
     }
 }
