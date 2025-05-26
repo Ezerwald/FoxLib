fe0968390e50fdd135e349d247d3414c0d29df22
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-05-26 13:28:10 +0200

    Add BookDatabase service
    Service for interacting with SQLite database
    - List books
    - Add books
    - Delete books

---
+++ FoxLib/Services/BookDatabase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SQLite;
+using FoxLib.Models;
+
+namespace FoxLib.Services
+{
+    public class BookDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public BookDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Book>().Wait();
+        }
+
+        public Task<List<Book>> GetBooksAsync()
+        {
+            return _database.Table<Book>().ToListAsync();
+        }
+
+        public Task<List<Book>> GetBooksByStatusAsync(ReadingStatus status)
+        {
+            return _database.Table<Book>().Where(b => b.Status == status).ToListAsync();
+        }
+
+        public Task<int> SaveBookAsync(Book book)
+        {
+            if (book.Id != 0)
+                return _database.UpdateAsync(book);
+            else
+                return _database.InsertAsync(book);
+        }
+
+        public Task<int> DeleteBookAsync(Book book)
+        {
+            return _database.DeleteAsync(book);
+        }
+    }
+}
