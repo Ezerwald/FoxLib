d5e68b16d7a5c8dbe0860378c17ca763f03561b0
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2025-05-26 13:46:08 +0200

    Add View Models
    - BaseViewModel
    - LibraryViewModel

--- FoxLib/App.xaml.cs
+++ FoxLib/App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace FoxLib
+﻿using FoxLib.Services;
+
+namespace FoxLib
 {
     public partial class App : Application
     {
+        private static BookDatabase _database;
+        public static BookDatabase Database
+        {
+            get
+            {
+                if (_database == null)
+                {
+                    var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FoxLib.db3");
+                    _database = new BookDatabase(dbPath);
+                }
+                return _database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
---
+++ FoxLib/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using FoxLib.Models;
+using System.Threading.Tasks;
+
+namespace FoxLib.ViewModels
+{
+    public class LibraryViewModel : BaseViewModel
+    {
+        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book>();
+
+        public LibraryViewModel()
+        {
+            LoadBooksCommand = new Command(async () => await LoadBooksAsync());
+        }
+
+        public ICommand LoadBooksCommand { get; }
+
+        private async Task LoadBooksAsync()
+        {
+            Books.Clear();
+            var books = await App.Database.GetBooksAsync();
+            foreach (var book in books)
+                Books.Add(book);
+        }
+    }
+}
+
